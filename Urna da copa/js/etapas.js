e974a6723d3dc109f14b1197e887b7528a614f48
Cláudio Cassiano <claudiocassiano@gmail.com>
2022-12-08 00:32:26 -0300

    Todas as pastas
    Inicio do projeto, todas as pastas anexadas.

---
+++ Urna da copa/js/etapas.js
@@ -0,0 +1,54 @@
+const etapas = [
+  
+
+  {
+    titulo: "MELHOR JOGADOR",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 11,
+        posicao: "Atacante",
+        pais: "AGENTINA",
+        fotos: [
+          { url: "Messi.png", legenda: "MESSI", isPequeno: false },
+        ],
+      },
+
+      {
+        numero: 12,
+        posicao: "Atacante",
+        pais: "PORTUGAL",
+        fotos: [
+            { url: "Cristiano.png", legenda: "C.RONALDO", isPequeno: false },
+        ],
+      },
+
+      {
+        numero: 13,
+        posicao: "Atacante",
+        pais: "BRASIL",
+        fotos: [
+            { url: "Neymar.png", legenda: "NEYMAR JR", isPequeno: false },
+        ],
+      },
+
+      {
+        numero: 14,
+        posicao: "Atacante",
+        pais: "URUGUAI",
+        fotos: [
+            { url: "Cavani.png", legenda: "CAVANI", isPequeno: false },
+        ],
+      },
+
+      {
+        numero: 15,
+        posicao: "Atacante",
+        pais: "FRANÇA",
+        fotos: [
+            { url: "Mbappe.png", legenda: "MBAPPE", isPequeno: false },
+        ],
+      },
+    ],
+  },
+];
